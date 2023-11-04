776cfc6cfd51b5bd748e1cb0ae486ed1499cdc6f
songhyun9999 <songhyun0323@konkuk.ac.kr>
2023-11-04 22:47:13 +0900

    구글 로그인 구현사항, 수정해야함

---
+++ app/src/main/java/com/example/thanksd/login/dataclass/ClientInformation.kt
@@ -0,0 +1,9 @@
+package com.example.thanksd.login.dataclass
+
+class ClientInformation (){
+    companion object {
+        val CLIENT_SECRET: String = ""
+        val CLIENT_ID: String = ""
+    }
+
+}
---
+++ app/src/main/java/com/example/thanksd/login/google/LoginRepository.kt
@@ -0,0 +1,60 @@
+package com.example.thanksd.login.google
+
+import android.util.Log
+import com.example.thanksd.login.dataclass.ClientInformation
+import com.example.thanksd.login.dataclass.LoginGoogleRequestModel
+import com.example.thanksd.login.dataclass.LoginGoogleResponseModel
+import com.example.thanksd.login.dataclass.SendAccessTokenModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class LoginRepository {
+    companion object {
+        const val TAG = "LoginRepository"
+    }
+
+    private val getAccessTokenBaseUrl = "https://www.googleapis.com"
+    private val sendAccessTokenBaseUrl = "server_base_url"
+
+    fun getAccessToken(authCode:String) {
+        LoginService.loginRetrofit(getAccessTokenBaseUrl).getAccessToken(
+            request = LoginGoogleRequestModel(
+                grant_type = "authorization_code",
+                client_id = ClientInformation.CLIENT_ID,
+                client_secret = ClientInformation.CLIENT_SECRET,
+                code = authCode.orEmpty()
+            )
+        ).enqueue(object : Callback<LoginGoogleResponseModel> {
+            override fun onResponse(call: Call<LoginGoogleResponseModel>, response: Response<LoginGoogleResponseModel>) {
+                if(response.isSuccessful) {
+                    val accessToken = response.body()?.access_token.orEmpty()
+                    Log.d(TAG, "accessToken: $accessToken")
+                    sendAccessToken(accessToken)
+                }
+            }
+            override fun onFailure(call: Call<LoginGoogleResponseModel>, t: Throwable) {
+                Log.e(TAG, "getOnFailure: ",t.fillInStackTrace() )
+            }
+        })
+    }
+
+
+    fun sendAccessToken(accessToken:String){
+        LoginService.loginRetrofit(sendAccessTokenBaseUrl).sendAccessToken(
+            accessToken = SendAccessTokenModel(accessToken)
+        ).enqueue(object :Callback<String>{
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful){
+                    Log.d(TAG, "sendOnResponse: ${response.body()}")
+                }
+            }
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "sendOnFailure: ${t.fillInStackTrace()}", )
+            }
+        })
+    }
+
+
+}
