0dddf6a5740cd0d658918001ecbbeac0ef0d1324
songhyun9999 <songhyun0323@konkuk.ac.kr>
2023-11-16 01:20:14 +0900

    구글 로그인 오류 수정

--- app/src/main/java/com/example/thanksd/login/google/GoogleLoginActivity.kt
+++ app/src/main/java/com/example/thanksd/login/google/GoogleLoginActivity.kt
@@ -13,23 +13,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.thanksd.BuildConfig
 import com.example.thanksd.MainPage.MainActivity
 import com.example.thanksd.httpconnection.HttpFunc
 import com.example.thanksd.login.LoginActivity
 import com.example.thanksd.login.google.ui.theme.ThanksDTheme
-import com.kakao.sdk.auth.Constants.REDIRECT_URI
+
+
 import org.json.JSONObject
 
 class GoogleLoginActivity : ComponentActivity() {
@@ -61,9 +58,9 @@ fun GoogleLoginScreen() {
     val url = "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id="+
             clientId+"&redirect_uri="+redirecturl+"&scope=email profile"
 
-//    val cookieManager = CookieManager.getInstance()
-//    cookieManager.removeAllCookies(null)
-//    cookieManager.flush()
+    val cookieManager = CookieManager.getInstance()
+    cookieManager.removeAllCookies(null)
+    cookieManager.flush()
     LaunchedEffect(check.value){
         if(check.value == 1){
             cur.startActivity(Intent(cur, MainActivity::class.java))
@@ -76,7 +73,8 @@ fun GoogleLoginScreen() {
         modifier = Modifier.fillMaxSize(),
         factory = { context ->
             WebView(context).apply {
-                webViewClient = object:WebViewClient(){
+                 // 기본 WebViewClient 사용
+                webViewClient = object: WebViewClient(){
                     override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
                         if (url.startsWith(redirecturl)) {
                             val uri = Uri.parse(url)
@@ -95,17 +93,14 @@ fun GoogleLoginScreen() {
                             }
                             else{
                                 Log.d("google_login","failed to get authcode")
+//                                check.value = 0
                             }
-//                            accessToken.value = "fail"
                             // 인증 코드를 처리하는 로직
                         }
-                        if(check.value !=1){
-                            check.value=0
-                        }
-//                        accessToken.value = "fail"
                         return false
                     }
-                } // 기본 WebViewClient 사용
+
+                }
                 val userAgentString = this.settings.userAgentString
                 val newUserAgentString = userAgentString
                     .replace("; wv", "")
@@ -113,8 +108,11 @@ fun GoogleLoginScreen() {
 //                this.settings.userAgentString ="Mozilla/5.0 AppleWebKit/535.19 Chrome/56.0.0 Mobile Safari/535.19"
                 this.settings.userAgentString = newUserAgentString
                 this.settings.javaScriptEnabled = true
-                loadUrl(url!!) // Google 로그인 URL
+                loadUrl(url!!)
+//                loadUrl("http://www.naver.com")
+            // Google 로그인 URL
 //                loadUrl("https://accounts.google.com/o/oauth2/v2/auth")
+//                Log.d("google_login", "check")
             }
 
         }
