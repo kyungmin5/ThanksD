9a0fba2ee9c205f13fc6f97c8eca2b40531460d1
Kyungmin5 <zzanggu0603@naver.com>
2023-11-22 17:08:59 +0900

    명언 모음집 및 asmr 페이지 구상

--- app/src/main/java/com/example/thanksd/MainPage/MainActivity.kt
+++ app/src/main/java/com/example/thanksd/MainPage/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.thanksd.MainPage
 
 import android.annotation.SuppressLint
+import android.net.http.HttpResponseCache.install
 import android.os.Bundle
 import android.widget.CalendarView
 import androidx.activity.ComponentActivity
@@ -9,19 +10,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
@@ -31,16 +30,22 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.google.android.material.color.ColorResourcesOverride
+import com.example.thanksd.BottomNavItem
+import com.example.thanksd.R
 
 
 class MainActivity : ComponentActivity() {
@@ -61,10 +66,16 @@ fun Calendar() {
     var date by remember {
         mutableStateOf("")
     }
+
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "Thanks :D") }
+                title = {
+                    Text(
+                        text = "Thanks :D",
+                        fontWeight = FontWeight.Bold // 굵은 글꼴 지정
+                    )
+                }
             )
         },
         content = {
@@ -72,9 +83,21 @@ fun Calendar() {
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
+                horizontalAlignment = CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
+                // 위쪽 고정 텍스트 창
+                Text(
+                    text = "Hi, user12! How's your day going? Here is Today's quotes for you :)",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Transparent, shape = RoundedCornerShape(8.dp))
+                        .padding(16.dp)
+                )
+
                 AndroidView(
                     factory = { context ->
                         CalendarView(context).apply {
@@ -83,25 +106,48 @@ fun Calendar() {
                         }
                     },
                     update = { calendarView ->
-                        calendarView.setOnDateChangeListener { calenderView, year, month, day ->
+                        calendarView.setOnDateChangeListener { _, year, month, day ->
                             date = "$year - ${month + 1} - $day"
                         }
                     },
                     modifier = Modifier
                         .aspectRatio(1f)
                         .border(3.dp, Color.Gray, shape = RoundedCornerShape(16.dp))
+                        .padding(bottom = 16.dp) // 캘린더뷰 아래 여백 추가
                 )
                 Text(text = date)
+
+                // 우측 하단에 흰색 동그라미 아이콘 추가
+                FloatingActionButton(
+                    onClick = {
+                        // 아이콘 클릭 시 수행할 동작 추가
+                    },
+                    modifier = Modifier
+                        .padding(16.dp, 0.dp, 16.dp, 16.dp) // + 버튼 여백 조절
+                        .size(56.dp)
+                        .background(Color.Transparent, shape = CircleShape)
+                        .border(1.dp, Color.LightGray, shape = CircleShape)
+                        .align(Alignment.End),
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = null,
+                        tint = Color.Gray, // '+' 모양의 색상 설정
+                        modifier = Modifier
+                            .size(24.dp) // 이미지 크기 조절
+                    )
+                }
             }
         }
     )
 }
 
+
 @Composable
 fun sample(){
     Column (
         modifier = Modifier.fillMaxSize().background(Color.White),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
         Text(text = "오류 방지용입니다")
@@ -223,5 +269,6 @@ fun Screen(navController: NavHostController){
 @Preview
 @Composable
 fun CalendarPreview() {
-    Calendar()
+    val navController = rememberNavController()
+    Screen(navController)
 }
