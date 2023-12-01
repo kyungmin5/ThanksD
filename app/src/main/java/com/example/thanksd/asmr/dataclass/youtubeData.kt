c4ae61c0edb0f21787822265f95c742bbdae94b2
songhyun9999 <songhyun0323@konkuk.ac.kr>
2023-12-02 02:51:04 +0900

    불러온 유튜브 데이터 저장

---
+++ app/src/main/java/com/example/thanksd/asmr/dataclass/youtubeData.kt
@@ -0,0 +1,8 @@
+package com.example.thanksd.asmr.dataclass
+
+import kotlinx.coroutines.flow.MutableStateFlow
+
+object youtubeData {
+    val youtueItems = ArrayList<mediaItem>()
+    var isReady = false
+}
