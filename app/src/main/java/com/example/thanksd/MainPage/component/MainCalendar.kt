7acf26ae57ee1430f37b007a7b783a55fb5b082d
songhyun9999 <songhyun0323@konkuk.ac.kr>
2023-12-04 02:40:25 +0900

    다이어리 읽기 수정

--- app/src/main/java/com/example/thanksd/MainPage/component/MainCalendar.kt
+++ app/src/main/java/com/example/thanksd/MainPage/component/MainCalendar.kt
@@ -42,6 +42,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.thanksd.MainPage.DiaryEntryActivity
 import com.example.thanksd.Retrofit.RetrofitManager
 import com.example.thanksd.dashboard.DaysOfWeekTitle
 import com.example.thanksd.dashboard.ui.theme.DarkBrown
