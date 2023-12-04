cbb14b41cdac38d53c24d928ed6b94de3dd01bca
youngjunkim <kyj0703@konkuk.ac.kr>
2023-12-04 10:33:02 +0900

    대시보드 당일 작성된 일기 클릭 시 일기 볼 수 있도록 변경 권한 인증 화면 텍스트 스타일 변경

--- app/src/main/java/com/example/thanksd/editor/camera/NoPermissionScreen.kt
+++ app/src/main/java/com/example/thanksd/editor/camera/NoPermissionScreen.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Camera
 import androidx.compose.material3.Button
@@ -16,7 +17,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.thanksd.editor.camera.ui.theme.ThanksDTheme
 
 @Composable
@@ -24,11 +29,15 @@ import com.example.thanksd.editor.camera.ui.theme.ThanksDTheme
     onRequestPermission: () -> Unit
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().padding(20.dp,0.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Please grant the permission to use the camera to use the core functionality of this app.")
+        Text(text = "Please grant the permission\nto use the camera\nto use the core functionality of this app.",
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(0.dp,20.dp))
         Button(onClick = onRequestPermission) {
             Icon(imageVector = Icons.Default.Camera, contentDescription = "Camera")
             Text(text = "Grant permission")
